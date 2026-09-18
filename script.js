fcc2a1393607a52d89286c1a0bef9a6cf1fbee42
udyprado-pixel <udy.prado@escola.pr.gov.br>
2026-09-18 08:49:41 -0300

    atualizeio script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+const botosuCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botoCurtir){
+    let curtiu = flase;
+    botaoCurtir.addEventListener("click", curtir);
+  function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtir===false){
+        contador.textContent++;
+        curtir = true;}
+        else{
+            contador.textContent--;
+            curtir = false;
+        }
+        
+    }
+});
+    
